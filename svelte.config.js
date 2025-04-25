6a6b47391c9232966d8a845f591c9042567b7333
Jack Weston <jackweston@Jacks-MacBook-Air.local>
2025-04-25 15:15:55 +0100

    Update

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,11 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Get the directory name of the current module
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +15,8 @@ const config = {
     mdsvex({
       extensions: ['.md'],
       layout: {
-        _: 'src/lib/layouts/RecipeLayout.svelte'
+        // Use the full absolute path instead of the alias
+        _: path.resolve(__dirname, './src/lib/layouts/RecipeLayout.svelte')
       }
     })
   ],
@@ -24,6 +30,10 @@ const config = {
     }),
     paths: {
       base: '/cookbook-chefstore'
+    },
+    alias: {
+      // Make sure the $lib alias is explicitly defined
+      '$lib': path.resolve(__dirname, './src/lib')
     }
   }
 };
