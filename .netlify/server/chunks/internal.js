236f08e08a884d2eec0717e6130bbfb3e45019fa
Jack Weston <jackweston@Jacks-MacBook-Air.local>
2025-05-07 14:20:15 +0100

    Reverting change

--- .netlify/server/chunks/internal.js
+++ .netlify/server/chunks/internal.js
@@ -543,7 +543,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1vfyrwa"
+  version_hash: "cema58"
 };
 async function get_hooks() {
   let handle;
