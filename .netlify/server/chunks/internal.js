8fc58a231c4d7369d2eb50befbdc4a93a43815e5
Jack Weston <jackweston@Jacks-MacBook-Air.local>
2025-05-13 14:30:09 +0100

    Favicon update

--- .netlify/server/chunks/internal.js
+++ .netlify/server/chunks/internal.js
@@ -543,7 +543,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "tu7i8k"
+  version_hash: "1wh0vsk"
 };
 async function get_hooks() {
   let handle;
--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.D7_F1shC.js","_app/immutable/chunks/mW9Xvd0U.js","_app/immutable/chunks/CeGAe43q.js","_app/immutable/chunks/DjPAE2bA.js","_app/immutable/chunks/CnPUFXdJ.js","_app/immutable/chunks/B1N_D3ZJ.js","_app/immutable/chunks/nJNMrujL.js"];
+export const imports = ["_app/immutable/nodes/1.BNl6ftdX.js","_app/immutable/chunks/mW9Xvd0U.js","_app/immutable/chunks/CeGAe43q.js","_app/immutable/chunks/DjPAE2bA.js","_app/immutable/chunks/Cox0606z.js","_app/immutable/chunks/B1N_D3ZJ.js","_app/immutable/chunks/nJNMrujL.js"];
 export const stylesheets = [];
 export const fonts = [];
